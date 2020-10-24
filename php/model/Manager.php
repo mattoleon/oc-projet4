36abafb06da345d51e864d756f88c26e4cfd202c
« Matthieu <matthieu.rebuffat@gmail.com>
2020-10-24 11:36:12 +0200

    Update manager

--- php/model/Manager.php
+++ php/model/Manager.php
@@ -6,7 +6,7 @@ class Manager
 {
     protected function dbConnect() // Connexion à la base de donnée
     {
-        //$db = new \PDO('mysql:host=db5001001057.hosting-data.io;dbname=dbs867532', 'dbu1059728', '');
+        
         $db = new \PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root', array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
         return $db;
     }
